c88d6d0d30dbddd0e587cbf01b19326e2eefc8e7
TriSetianto123 <156288410+3nol04@users.noreply.github.com>
2024-12-08 12:24:46 +0700

    new main.dart

--- thriftcycle/lib/main.dart
+++ thriftcycle/lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:thriftcycle/wigedts/splashscreen.dart';
+import '../screens/homepage.dart';
+import '../screens/detail_product.dart';
+import '../screens/time_line.dart';
+import '../wigedts/steps_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MaterialApp(
+      //home : StepsProgres(),
+      //home: TimeLine(),
+     // home: HomePage(),
+      //home:DetailProduct()
+      home:Splashscreen()
+
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const Scaffold();
   }
 }
