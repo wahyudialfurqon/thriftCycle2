a48cdb15c4db79b0a52c321d0b05f650ded29cce
TriSetianto123 <156288410+3nol04@users.noreply.github.com>
2024-12-31 13:07:51 +0700

    uploadScreen

--- thriftcycle/lib/main.dart
+++ thriftcycle/lib/main.dart
@@ -16,6 +16,7 @@ void main() {
         //home:DetailProduct()
         /// home: Splashscreen(),
         home : UploadScreen(),
+        //home : ProfileScreen(),
     
   ));
 }
@@ -36,7 +37,7 @@ class _HomeState extends State<Home> {
     Center(child: Text('Search Page')),
     Center(child: Text('Add Photo Page')),
     Center(child: Text('Bookmark Page')),
-   const ProfileScreen(),
+  
   ];
 
   @override
