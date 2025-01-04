c82ada9e99a6eb0156b0c5183b142f666a7a1978
TriSetianto123 <156288410+3nol04@users.noreply.github.com>
2025-01-04 16:58:27 +0700

    hapus folder thriftcycle

--- thriftcycle/android/app/src/main/kotlin/com/example/thriftcycle/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.thriftcycle
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- thriftcycle/lib/service/model/category.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class Category {
-  final int id;
-  final String name;
-  final IconData icon;
-
-  Category({required this.id, required this.name, required this.icon});
-}
